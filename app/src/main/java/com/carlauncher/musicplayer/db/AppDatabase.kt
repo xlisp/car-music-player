34daaffa24989ea699ea20d202e3f31e5a56e136
xlisp <xlisp369@gmail.com>
2026-04-02 20:09:02 +0800

    update

--- app/src/main/java/com/carlauncher/musicplayer/db/AppDatabase.kt
+++ app/src/main/java/com/carlauncher/musicplayer/db/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [PlaylistEntity::class, PlaylistSongEntity::class], version = 1)
+@Database(entities = [PlaylistEntity::class, PlaylistSongEntity::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun playlistDao(): PlaylistDao
